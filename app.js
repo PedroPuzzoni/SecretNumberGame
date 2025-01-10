c5ae5e3eaf8ff156ac0c5b49735e607b63af2562
Pedro Puzzoni <pedrolucapuzzoni@gmail.com>
2025-01-10 15:40:06 -0300

    Initial Project

---
+++ app.js
@@ -0,0 +1,45 @@
+alert('Bem-vindo(a) ao jogo do Número Secreto!');
+
+let maxNumber = 5000;
+let secretNumber = parseInt(Math.random() * maxNumber + 1);
+console.log(secretNumber);
+let userGuess;
+let userAttempts = 1;
+
+
+//Enquanto o userguess não for igual ao numero secreto
+while (userGuess != secretNumber) {
+
+    userGuess = prompt(`Escolha entre o número 1 e ${maxNumber}.`);
+
+    //Se o chute do usuário for igual ao número secreto e se não for.
+    if (userGuess == secretNumber) {
+        break;
+   
+ 
+    } else {
+        if(userGuess > secretNumber) {
+ 
+     alert(`O número secreto é menor que (${userGuess})`);
+    } else {
+ 
+ 
+     alert(`O número secreto é maior que (${userGuess})`);
+ 
+  }
+
+ }
+
+
+ // userAttempts = userAttempts + 1;
+ userAttempts++;
+
+}
+
+let wordAttempt = userAttempts > 1 ? 'tentativas' : 'tentativa';
+
+alert(`Parabéns! Você descobriu o número secreto (${secretNumber}) com ${userAttempts} ${wordAttempt}.`);
+
+
+
+
